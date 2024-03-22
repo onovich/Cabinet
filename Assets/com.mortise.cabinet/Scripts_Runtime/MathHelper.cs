b0d8beb826ca1a3a9ac4f2d0ddcd651eb6d13d7e
onovich <hjn1110@163.com>
2024-03-23 03:45:27 +0800

    <feature> modify: Quick Sort Function

--- Assets/com.mortise.cabinet/Scripts_Runtime/MathHelper.cs
+++ Assets/com.mortise.cabinet/Scripts_Runtime/MathHelper.cs
@@ -2,48 +2,51 @@ using System;
 using System.Collections.Generic;
 
 namespace MortiseFrame.Cabinet {
-
     internal static class MathHelper {
-
         internal class DefaultComparer<T> : Comparer<T> where T : IComparable<T> {
             public override int Compare(T x, T y) {
                 return x.CompareTo(y);
             }
         }
 
-        internal static void QuickSort<T>(T[] src, int offset, int count, Comparer<T> comparer = null) where T : IComparable<T> {
-            if (count <= 1) return;
-
+        internal static void QuickSort<T>(List<T> src, Comparer<T> comparer = null) where T : IComparable<T> {
             comparer ??= new DefaultComparer<T>();
-
-            int pivotIndex = Partition(src, offset, count, comparer);
-            QuickSort(src, offset, pivotIndex - offset, comparer);
-            QuickSort(src, pivotIndex + 1, offset + count - pivotIndex - 1, comparer);
+            QuickSortInternal(src, 0, src.Count - 1, comparer);
         }
 
-        static int Partition<T>(T[] arr, int offset, int count, Comparer<T> comparer) where T : IComparable<T> {
-            T pivot = arr[offset];
-            int i = offset, j = offset + count - 1;
-
-            while (i < j) {
-                while (i < offset + count - 1 && comparer.Compare(arr[i], pivot) <= 0) i++;
-                while (j > offset && comparer.Compare(arr[j], pivot) > 0) j--;
-
-                if (i < j) {
-                    Swap(arr, i, j);
-                }
+        static void QuickSortInternal<T>(List<T> src, int left, int right, Comparer<T> comparer) where T : IComparable<T> {
+            if (left < right) {
+                int pivotIndex = Partition(src, left, right, comparer);
+                QuickSortInternal(src, left, pivotIndex - 1, comparer);
+                QuickSortInternal(src, pivotIndex + 1, right, comparer);
             }
-
-            Swap(arr, offset, j);
-            return j;
         }
 
-        static void Swap<T>(T[] arr, int i, int j) where T : IComparable<T> {
-            T temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        static int Partition<T>(List<T> arr, int left, int right, Comparer<T> comparer) where T : IComparable<T> {
+            T pivot = arr[left];
+            int i = left - 1;
+            int j = right + 1;
+
+            while (true) {
+                do {
+                    i++;
+                } while (comparer.Compare(arr[i], pivot) < 0);
+
+                do {
+                    j--;
+                } while (comparer.Compare(arr[j], pivot) > 0);
+
+                if (i >= j) return j;
+
+                Swap(arr, i, j);
+            }
         }
 
+        static void Swap<T>(List<T> src, int i, int j) {
+            T temp = src[i];
+            src[i] = src[j];
+            src[j] = temp;
+        }
     }
 
 }
